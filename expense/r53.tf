ea148a5a6bfafc0b05608fa86a1311e9ae296003
kedarnath9988 <anubolukedarnath9988@gmail.com>
2025-07-17 11:28:33 +0530

    patching of the locals in expense

--- expense/r53.tf
+++ expense/r53.tf
@@ -1,9 +1,9 @@
 resource "aws_route53_record" "expense" {
   count = length(var.instance_names)
   zone_id = var.host_zone_id
-  name    = local.record_name
+  name    = var.instance_names[count.index] == "frontend" ? var.domain_name : "${var.instance_names[count.index]}.${var.domain_name}"
   type    = "A"
   ttl     = 1
-  records = local.records_value_ip 
+  records = records_value_ip = [aws_instance.expense[count.index] == "frontend" ? aws_instance.expense[count.index].public_ip : aws_instance.expense[count.index].private_ip]
   allow_overwrite = true
 }
